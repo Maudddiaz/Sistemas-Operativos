295c05e526da291eb230b9ec7a4ffbcef523acd8
maudddiaz <mauroapp7@gmail.com>
2025-10-03 11:55:35 -0300

    ya esta una parte la de elegir el primer proceso como referencia

--- módulo1.py
+++ módulo1.py
@@ -1,7 +1,7 @@
-Contador_P = 0
-memoria = [100,250,150,50]
+Contador_P = 0 #contador de programa segun el diagrama de flujo
+memoria = [100,250,150,50]#la memoria fija que cada vez que este algo ahi se le resta el tamaño del proceso
 
-class Proceso:
+class Proceso:#defino por asi decirlo el registro de proceso
     def __init__(self, tamaño, tiempo):
         self.tamaño = tamaño
         self.tiempo = tiempo 
@@ -9,11 +9,19 @@ class Proceso:
 nuevo = []
 listo = []
 l_s = []
-ejecucion = []
+ejecucion = []#no se cuantos procesos se pueden tener en ejecucion a la vez esto queda hablarlo
 Terminado = []
 
-nuevo.append(Proceso(50,3))
+nuevo.append(Proceso(50,3))# el append lo que hace es añadir un nuevo elemento en ese arreglo dinamico
 nuevo.append(Proceso(100,1))
 
-if Contador_P = 10 
+for particiones in reversed(memoria): #el reversed basicamente lo que hace es recorrer el arreglo de atras para adelante
+    if particiones <> 1 
+        if nuevo[1].tamaño <= memoria[particiones]:#nuevo[1].tamaño es el primer Proceso en la cola   y lo tomo para agarrar la primera referencia
+            memoria[particiones]:= memroia[particiones] - nuevo[1].tamaño
+            listo.append(nuevo[1])
+            break#basicamente me sale del ciclo for
+    
+if Contador_P <> 10
+    
     
