ec640f9dda6db37703817feed7ebf3b15140ce2c
maudddiaz <mauroapp7@gmail.com>
2025-10-03 12:34:52 -0300

    el commit anterior era trucho

--- módulo1.py
+++ módulo1.py
@@ -1,27 +1,40 @@
 Contador_P = 0 #contador de programa segun el diagrama de flujo
-memoria = [100,250,150,50]#la memoria fija que cada vez que este algo ahi se le resta el tamaño del proceso
 
-class Proceso:#defino por asi decirlo el registro de proceso
+
+class Proceso:#defino por asi decirlo el registro de proceso como en pseudocodigo
     def __init__(self, tamaño, tiempo):
         self.tamaño = tamaño
-        self.tiempo = tiempo 
+        self.tiempo = tiempo
 
-nuevo = []
-listo = []
-l_s = []
+class Memoriaparticion:
+    def __init__(self,tamañooriginal,tamañoactual,estado):
+        self.tamañooriginal = tamañooriginal
+        self.tamañoactual   = tamañoactual
+        self.estado = estado # estado ocupado o desacupado son 
+
+memoria =[]
+nuevo = []#no ocupa memoria
+listo = []# habrian 2 aqui y el 3ero supongo que estaria en ejecucion
+l_s = []# no ocupa memoria los listos suspendidos
 ejecucion = []#no se cuantos procesos se pueden tener en ejecucion a la vez esto queda hablarlo
-Terminado = []
+Terminado = []# no ocupa me moria
+
+memoria.append(memoriaparticion(100,100,libre))
+memoria.append(memoriaparticion(250,250,libre))
+memoria.append(memoriaparticion(150,150,libre))
+memoria.append(memoriaparticion(50,50,libre)) #god
+
 
 nuevo.append(Proceso(50,3))# el append lo que hace es añadir un nuevo elemento en ese arreglo dinamico
 nuevo.append(Proceso(100,1))
 
 for particiones in reversed(memoria): #el reversed basicamente lo que hace es recorrer el arreglo de atras para adelante
     if particiones <> 1 
-        if nuevo[1].tamaño <= memoria[particiones]:#nuevo[1].tamaño es el primer Proceso en la cola   y lo tomo para agarrar la primera referencia
-            memoria[particiones]:= memroia[particiones] - nuevo[1].tamaño
+        if nuevo[1].tamaño <= memoria.tamañooriginal[particiones]:#nuevo[1].tamaño es el primer Proceso en la cola   y lo tomo para agarrar la primera referencia
+            memoria.tamañoactual[particiones] = memoria.tamañoactual[particiones] - nuevo[1].tamaño
             listo.append(nuevo[1])
             break#basicamente me sale del ciclo for
     
-if Contador_P <> 10
+if Contador_P <> 10 # nose como seguir :v
     
     
