743f7020c87c4f95fcf8a09e93bf0ece28da5a61
maudddiaz <mauroapp7@gmail.com>
2025-10-02 23:34:58 -0300

    el inicio del proyecto lo de los arrays dinamicos y eso

---
+++ módulo1.py
@@ -0,0 +1,19 @@
+Contador_P = 0
+memoria = [100,250,150,50]
+
+class Proceso:
+    def __init__(self, tamaño, tiempo):
+        self.tamaño = tamaño
+        self.tiempo = tiempo 
+
+nuevo = []
+listo = []
+l_s = []
+ejecucion = []
+Terminado = []
+
+nuevo.append(Proceso(50,3))
+nuevo.append(Proceso(100,1))
+
+if Contador_P = 10 
+    
